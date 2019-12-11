e8a6321167a902580415bd30fd50e5ab7426f9dd
manish0345 <manish.soni@krishtechnolabs.com>
2019-12-11 12:55:04 +0530

    changes

---
+++ pub/static/adminhtml/Magento/backend/en_US/Magento_CurrencySymbol/js/symbols-form.js
@@ -0,0 +1,1 @@
+/var/www/html/magento/vendor/magento/module-currency-symbol/view/adminhtml/web/js/symbols-form.js
---
+++ pub/static/frontend/Magento/luma/en_US/Eadesigndev_RomCity/js/view/shipping.js
@@ -0,0 +1,1 @@
+/var/www/html/magento/app/code/Eadesigndev/RomCity/view/frontend/web/js/view/shipping.js
